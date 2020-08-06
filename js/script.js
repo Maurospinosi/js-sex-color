2aa43d8cf0bcf20dcd57ee322126d1f154e54d47
Mauro <mauro.spinosi94@gmail.com>
2020-08-06 17:41:40 +0200

    Color

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 //Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi. Sulla pagina html deve apparire “Ciao <nome>”, il colore del nome deve essere azzurro o rosa a seconda del sesso inserito.
+var nome = prompt("Inserisci il nome");
+var sesso = prompt("Inserisci il sesso")
+document.getElementById('testo').innerHTML =" " + nome;
+
+
+if (sesso = "maschio") {
+  var nome = document.getElementById('testo');
+  nome.style.color = "blue";
+} else if (sesso = "femmina") {
+  var nome = document.getElementById('testo');
+  nome.style.color = "pink";
+}
